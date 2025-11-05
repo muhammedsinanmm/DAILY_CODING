3fcffcaea62c44c2e93f79121db931a021f9193a
muhammedsinanmm <muhamedh.sinan@gmail.com>
2025-11-05 15:19:27 +0530

    logic resolved with root N method and added the edge case if number is one

--- primeNumbersinRange.py
+++ primeNumbersinRange.py
@@ -1,14 +1,16 @@
 def prime(number):
+    if(number==1):
+        return False
     i = 2
-    while i<=number//2:
+    while i*i <= number:
         if number%i == 0:
             return False
         i+=1
     return True
 start = int(input("Enter the starting value: "))
 end = int(input("Enter the ending value: "))
-sum = 0
+total = 0
 for i in range(start,end+1):
     if prime(i):
-        sum+=i
-print(f"The sum of prime numbers from {start} to {end} is {sum}")
+        total+=i
+print(f"The sum of prime numbers from {start} to {end} is {total}")
